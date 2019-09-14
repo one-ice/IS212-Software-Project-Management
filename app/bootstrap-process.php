44dc5a2d955cb6a8c75e473bdf5b724ed987a68c
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2019-09-14 11:50:01 +0800

    Initial push of documents
    Backbone of project

---
+++ app/bootstrap-process.php
@@ -0,0 +1,4 @@
+<?php
+# edit the file included below. the bootstrap logic is there
+require_once 'include/bootstrap.php';
+doBootstrap();
---
+++ app/json/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../include/bootstrap.php';
+
+# complete bootstrap
+doBootstrap();
