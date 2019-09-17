6ec171634b1dcac40b4087aab942b812d4f2d298
one-ice <53385835+one-ice@users.noreply.github.com>
2019-09-17 18:16:33 +0800

    Update bootstrap.php
    Created function to check for empty column

--- app/include/bootstrap.php
+++ app/include/bootstrap.php
@@ -1,4 +1,19 @@
 <?php
 require_once 'common.php';
 
+#Use this function to check for empty columns. (See below)
+#This function returns an ARRAY of error message for any empty fields
+#E.g ["Blank Title", "Blank Description"]
+function checkForEmptyCol( $data, $header){
+    $errors = [];
+    for ($i=0; $i < sizeof($data); $i++) { 
+        if (empty($data[$i])){
+            $fieldname = "Blank " . $header[$i];
+            array_push($errors , $fieldname);
+        }
+    }
+    return $errors;
+}
+
+
 ?>
