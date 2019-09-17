5c6b0c043cc3a7f9d15c057562a50d4fd29c2b6f
one-ice <53385835+one-ice@users.noreply.github.com>
2019-09-17 18:29:26 +0800

    Update bootstrap.php
    Starting on prerequisite bootstrap

--- app/include/bootstrap.php
+++ app/include/bootstrap.php
@@ -24,4 +24,55 @@ function removeWhiteSpace($data){
     return $arrayToReturn;
 }
 
+function doBootstrap() {
+
+    $errors = array();
+	# need tmp_name -a temporary name create for the file and stored inside apache temporary folder- for proper read address
+	$zip_file = $_FILES["bootstrap-file"]["tmp_name"];
+
+	# Get temp dir on system for uploading
+	$temp_dir = sys_get_temp_dir();
+
+    # keep track of number of lines successfully processed for each file
+    #Add your processed count here
+	$prereq_processed=0;
+
+	# check file size
+	if ($_FILES["bootstrap-file"]["size"] <= 0)
+		$errors[] = "input files not found";
+
+	else {
+	
+		$zip = new ZipArchive;
+		$res = $zip->open($zip_file);
+
+		if ($res === TRUE) {
+			$zip->extractTo($temp_dir);
+			$zip->close();
+            
+            #Add your temp directory here
+			$prereq_path = "$temp_dir/prerequisite.csv";
+            
+            #Add your @fopen here
+			$prereq = @fopen($prereq_path, "r");
+            
+            #Add your emptys here
+            # empty($prereq) || empty($course) ||.........
+			if (empty($prereq) ){
+                $errors[] = "input files not found";
+                
+                #add @unlinks and fclose
+				if (!empty($prereq)){
+					fclose($prereq);
+					@unlink($prereq_path);
+                } 
+                
+			}
+			else {
+				$connMgr = new ConnectionManager();
+				$conn = $connMgr->getConnection();
+            }
+        }
+    }
+}
 ?>
