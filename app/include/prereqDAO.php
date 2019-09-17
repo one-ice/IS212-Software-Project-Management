25ea0d68483905165e7a585b0fe0349cefcaecfa
one-ice <53385835+one-ice@users.noreply.github.com>
2019-09-17 19:03:29 +0800

    Update prereqDAO.php

--- app/include/prereqDAO.php
+++ app/include/prereqDAO.php
@@ -18,6 +18,26 @@ class PrereqDAO{
 
         return $result;
     }  
+    public function retireve($course){
+        $sql = 'SELECT course, type FROM prerequisite WHERE course=:course';
+        
+        
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->bindParam(':course', $course, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $result = [];
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $result = new course($row['course'],$row['prerequisite']);
+        }
+
+        return $result;
+    }
+
 
 
 }
