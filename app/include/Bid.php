66286e709c39c5a4d8fcf1c8bbb335a1c16dcd11
Leonard Wong <leothelion96@users.noreply.github.com>
2019-09-18 20:27:59 +0800

    Initial commit on Bid.php
    Creation of Bid Class

---
+++ app/include/Bid.php
@@ -0,0 +1,20 @@
+<?php
+
+#Class for Bid
+class Bid {
+	public $userid;
+	public $amount;
+	public $code;
+	public $section;
+
+	
+	public function __construct($userid = '', $amount = '', $code = '', $section = '') {
+		$this->userid = $userid;
+		$this->amount = $amount;
+		$this->code = $code;
+		$this->section = $section;
+	}
+
+}
+
+?>
