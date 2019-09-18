f9d37b6d177a7633656389eda0a86b08b3683fec
Leonard Wong <leothelion96@users.noreply.github.com>
2019-09-18 20:35:09 +0800

    Initial commit of BidDAO.php
    Creation of BidDAO.php

---
+++ app/include/BidDAO.php
@@ -0,0 +1,71 @@
+<?php
+
+class BidDAO {
+
+    public  function retrieveAll() {
+        $sql = 'SELECT * FROM bid ORDER BY `amount`';
+        
+            
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        $result = array();
+
+        while($row = $stmt->fetch()) {
+            $result[] = new bid($row['userid'], $row['amount'], $row['code'], $row['section']);
+        }
+            
+                 
+        return $result;
+    }
+    
+  
+    public  function retrieve($userid) {
+        $sql = 'SELECT userid, amount FROM bid WHERE userid=:userid';
+        
+        
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->bindParam(':userid', $userid, PDO::PARAM_STR);
+        $stmt->execute();
+
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $result = new bid($row['userid'], $row['amount'], $row['code'], $row['section']);
+        }
+        
+        return $result;
+    }
+  
+  
+    public function add($bid) {
+        $sql = 'INSERT IGNORE INTO bid (userid, amount, code, section) VALUES (:userid, :amount, :code, :section)';
+        
+        $connMgr = new ConnectionManager();       
+        $conn = $connMgr->getConnection();
+         
+        $stmt = $conn->prepare($sql); 
+
+        $stmt->bindParam(':userid', $bid->userid, PDO::PARAM_STR);
+        $stmt->bindParam(':amount', $bid->amount, PDO::PARAM_INT);
+		$stmt->bindParam(':code', $bid->code, PDO::PARAM_STR);
+		$stmt->bindParam(':section', $bid->section, PDO::PARAM_STR);
+        
+        $isAddOK = False;
+		
+        if ($stmt->execute()) {
+            $isAddOK = True;
+        }
+
+        return $isAddOK;
+    }
+    
+        
+    
+}
