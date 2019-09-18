a9a56798b4e5ae61840c2419891b1e68e2f20dfe
SeeHoe <shlai.2018@smu.edu.sg>
2019-09-18 12:38:30 +0800

    Uploaded Couse And Section Class
    Course and Section Class finished to have a constructor as well as its attributes

---
+++ app/include/section.php
@@ -0,0 +1,25 @@
+<?php
+class section{
+    public $course;
+    public $section;
+    public $day;
+    public $start;
+    public $end;
+    public $instructor;
+    public $venue;
+    public $size;
+
+    public function __construct($course='', $section='',$day='',$start='',$end='',$instructor='',
+    $venue='',$size='') {
+        $this->course = $course;
+        $this->section = $section;
+        $this->day = $day;
+        $this->start = $start;
+        $this->end = $end;
+        $this->instructor = $instructor;
+        $this->venue = $venue;
+        $this->size = $size;
+    }
+}
+
+?>
