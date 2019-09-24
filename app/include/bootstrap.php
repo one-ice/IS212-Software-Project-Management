f777854bb5432bb172b3d4ce17218edf554e73e3
one-ice <53385835+one-ice@users.noreply.github.com>
2019-09-24 17:26:08 +0800

    Update bootstrap about course validation

--- app/include/bootstrap.php
+++ app/include/bootstrap.php
@@ -165,11 +165,14 @@ function doBootstrap() {
                         ];
                         array_push($errors , $errorDetails);
                     }
-            
-                    // elseif(){
-                    // add validation!
-                    // }
-            
+                    elseif(sizeof(checkCourseVali($data[2], $data[3],$data[4], $data[5], $data[6])) != 0){
+                        $errorInRow = checkCourseVali($data[2], $data[3],$data[4], $data[5], $data[6]);
+                        $errorDetails = [
+                            "file" => "course.csv",
+                            "line" => $lineCount,
+                            "message" => $errorInRow
+                        ];
+                    }
                     else{
                         $courseObj = new course($data[0], $data[1], $data[2], $data[3], 
                         $data[4], $data[5], $data[6]);
