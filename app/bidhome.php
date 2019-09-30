4f213b9a0053af09ff6cfa6074b6b142a4aced1c
serene98 <serene.ang.2018@sis.smu.edu.sg>
2019-09-30 22:47:29 +0800

    Update bidhome.php
    Update bidhome.php to display courses offered by user's school for round 1 bidding.
    
    Co-Authored-By: one-ice <one-ice@users.noreply.github.com>

--- app/bidhome.php
+++ app/bidhome.php
@@ -4,6 +4,7 @@ include_once "include/common.php";
 $username = 'amy.ng.2009';
 $round = 1;
 $studentDAO =  new StudentDAO();
+#retrieve student's edollar
 $studentedollar = ($studentDAO->retrieve($username))->edollar;
 echo "<h1> Welcome to BIOS </h1>";
 echo "<h3> Round $round </h3>";
@@ -18,5 +19,19 @@ if ($round == 1)
     $courseDAO = new CourseDAO();
     $courses = $courseDAO->retrieveAllbySchool($studentschool);
 
+    echo "<table border = '1'> <th> Course </th> <th> School </th> <th> Title </th> <th> Description </th>
+    <th> Exam Date </th> <th> Exam Start </th> <th> Exam End </th> <td> </td>";
+    foreach ($courses as $course)
+    {
+        echo "<tr> <td> {$course->course} </td>
+                <td> {$course->school} </td>
+                <td> {$course->title} </td>
+                <td> {$course->description} </td>
+                <td> {$course->exam_date} </td>
+                <td> {$course->exam_start} </td>
+                <td> {$course->exam_end} </td>
+                <td> <a href = 'bid.php?course={$course->course}'> Select </a> </td> </tr>";
+    }
+    echo "</table>";
 }
 ?>
