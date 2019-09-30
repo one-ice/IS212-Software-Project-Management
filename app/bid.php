6cddbca2d16f1c77236de81f856ec8df999265e0
serene98 <serene.ang.2018@sis.smu.edu.sg>
2019-09-30 22:53:48 +0800

    Update bid.php
    Update bid.php - create form for user to key in bid details
    
    Co-Authored-By: one-ice <one-ice@users.noreply.github.com>

--- app/bid.php
+++ app/bid.php
@@ -7,5 +7,27 @@ $studentDAO =  new StudentDAO();
 #retrieve student's edollar
 $studentedollar = ($studentDAO->retrieve($username))->edollar;
 echo "<p> You have $$studentedollar.</p>";
+?>
+
+<html>
+    <body> <form method = 'post'>
+<?php
+echo "Course: <label for ='$course'>$course</label> <br>";
+echo "Section: <input type = 'text' name = 'section'></input><br>";
+echo "Bid Amount: <input type = 'text' name = 'bid_amt'></input> <br>";
+
+echo "<input type = 'submit' name = 'back' value = 'Back'> </input>";
+echo "<input type = 'submit' name = 'submit' value = 'Submit'></input>";
+?>
+    </form> </body> 
+</html>
+
+<?php
+
+if (isset($_POST['back']))
+{
+    header("Location: bidhome.php");
+    return;
+}
 
 ?>
