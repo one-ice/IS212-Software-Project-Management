b4613a55c7fba8f98613d10ad618f45c21f1331b
serene98 <serene.ang.2018@sis.smu.edu.sg>
2019-09-30 22:43:06 +0800

    Update bidhome.php
    Update bidhome.php for bidding round 1
    
    Co-Authored-By: one-ice <one-ice@users.noreply.github.com>

--- app/bidhome.php
+++ app/bidhome.php
@@ -9,4 +9,14 @@ echo "<h1> Welcome to BIOS </h1>";
 echo "<h3> Round $round </h3>";
 echo "<p> You have $$studentedollar.</p>";
 
+if ($round == 1)
+{
+    #Retrieve user's school
+    $studentschool = ($studentDAO->retrieve($username))->school;
+
+    #Retrieve courses by school
+    $courseDAO = new CourseDAO();
+    $courses = $courseDAO->retrieveAllbySchool($studentschool);
+
+}
 ?>
