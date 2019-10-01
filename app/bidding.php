7969b63782324b8564aff60fffa5ab1bd7d72543
one-ice <53385835+one-ice@users.noreply.github.com>
2019-10-01 19:44:40 +0800

    Update bidding.php
    add function "meetCriteria"
    
    Co-Authored-By: serene98 <serene98@users.noreply.github.com>

--- app/bidding.php
+++ app/bidding.php
@@ -1,23 +1,26 @@
 <?php
 include_once "include/common.php"; 
+include_once "meetCriteria.php";
 
 $course = $_GET['course'];
-$username = 'amy.ng.2009';
+$username = $_SESSION['username'];
 $studentDAO =  new StudentDAO();
-#retrieve student's edollar
 $studentedollar = ($studentDAO->retrieve($username))->edollar;
-echo "<p> You have $$studentedollar.</p>";
-?>
+if (!isset($_POST['submit']))
+{
+    echo "<p> You have $$studentedollar.</p>";
 
-<html>
-    <body> <form method = 'post'>
-<?php
-echo "Course: <label for ='$course'>$course</label> <br>";
-echo "Section: <input type = 'text' name = 'section'></input><br>";
-echo "Bid Amount: <input type = 'text' name = 'bid_amt'></input> <br>";
+    ?>
+    <html>
+        <body> <form method = 'post'>
+    <?php
+    echo "Course: <label for ='$course'>$course</label> <br>";
+    echo "Section: <input type = 'text' name = 'section'></input><br>";
+    echo "Bid Amount: <input type = 'text' name = 'bid_amt'></input> <br>";
 
-echo "<input type = 'submit' name = 'back' value = 'Back'> </input>";
-echo "<input type = 'submit' name = 'submit' value = 'Submit'></input>";
+    echo "<input type = 'submit' name = 'back' value = 'Back'> </input>";
+    echo "<input type = 'submit' name = 'submit' value = 'Submit'></input>";
+}
 ?>
     </form> </body> 
 </html>
@@ -30,4 +33,44 @@ if (isset($_POST['back']))
     return;
 }
 
+if(isset($_POST['submit']))
+{
+    $errors = meetCriteria($_SESSION['username'],$_POST['bid_amt'],$course,$_POST['section'],"Round 1");
+    $bid_amt = $_POST['bid_amt'];
+    $bid_section = $_POST['section'];
+    // $errors = isMeetCriteria();
+
+    if ($errors == [])
+    {
+        $bid = new Bid();
+        $bid->userid = $username;
+        $bid->amount = $bid_amt;
+        $bid->code = $course;
+        $bid->section = $bid_section;
+
+        #Add bid
+        $bidDAO = new BidDAO();
+        $status = $bidDAO->add($bid);
+        
+        if($status == True)
+        {
+
+            $amount_left = $studentedollar - $bid_amt;
+            echo "<p> Bid placed successfully! 
+                      Amount left: $$amount_left
+                </p>";
+            $studentDAO->update($username, $amount_left);
+        }
+    }
+    else{
+            echo "<li>";
+            foreach($errors as $error)
+            {   
+                echo "<ul> $error </ul>";
+            }
+            echo "</li>";
+    }
+
+}
 ?>
+
