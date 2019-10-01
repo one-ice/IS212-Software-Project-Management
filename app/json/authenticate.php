de6df60f497632414cc2a80d5626cf1c8d107abc
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2019-10-01 15:23:58 +0800

    Update authenticate.php

--- app/json/authenticate.php
+++ app/json/authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../app/include/common.php';
-require_once '../app/include/token.php';
+require_once '../include/common.php';
+require_once '../include/token.php';
 
 
 // isMissingOrEmpty(...) is in common.php
@@ -17,37 +17,28 @@ if (!isEmpty($errors)) {
         ];
 }
 else{
-    $username = $_POST['username'];
+    $userid = $_POST['username'];
     $password = $_POST['password'];
 
-# complete authenticate API
-
-    # check if username and password are right. generate a token and return it in proper json format
-    $userDao = new UserDAO();
-
-    $user = $userDao->retrieve($username);
-
+    $studentDao = new StudentDAO();
+    $student = $studentDao->retrieve($userid);
     
-    if($user != null && $user->authenticate($password)){
-        # after you are sure that the $username and $password are correct, you can do 
-
-        # generate a secret token for the user based on their username
-        $generatedToken = generate_token($username);
-
-        # return the token to the user via JSON
+    if($student != null && $student->authenticate($password)){
+        $generatedToken = generate_token($userid);
         $result = [
             "status" => "success",
-            "token" => $generatedToken
+            "token" => $generatedToken,
         ];
-
     }else{
-        # return error message if something went wrong
-        $result = ["status" => "error", "messages" => array_values(["invalid username/password"])];
+        $result = [
+            "status" => "error",
+            "messages" => array_values(["invalid username/password"])
+        ];
     }
 
 }
 
 header('Content-Type: application/json');
 echo json_encode($result, JSON_PRETTY_PRINT);
- 
+
 ?>
