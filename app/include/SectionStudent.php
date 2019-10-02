c115302ec7644ccd77b2d8033377a2ef7fd0db36
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2019-10-02 15:23:17 +0800

    Create SectionStudent.php
    New class for new section student table
    
    Co-Authored-By: seehoe <seehoe@users.noreply.github.com>

---
+++ app/include/SectionStudent.php
@@ -0,0 +1,18 @@
+<?php
+
+class SectionStudent {
+	public $userid;
+	public $code;
+	public $section;
+	public $amount;
+	
+	public function __construct($userid = '', $code = '', $section = '', $amount = '') {
+		$this->userid = $userid;
+		$this->code = $code;
+        $this->section = $section;		
+        $this->amount = $amount;
+	}
+
+}
+
+?>
