d6abd5388ebeea3ad6d6be2638d64f9d6da4c377
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2019-10-02 16:54:20 +0800

    Update Bid.php with Status
    Co-Authored-By: seehoe <seehoe@users.noreply.github.com>

--- app/include/Bid.php
+++ app/include/Bid.php
@@ -6,13 +6,15 @@ class Bid {
 	public $amount;
 	public $code;
 	public $section;
+	public $status;
 
 	
-	public function __construct($userid = '', $amount = '', $code = '', $section = '') {
+	public function __construct($userid = '', $amount = '', $code = '', $section = '', $status = '') {
 		$this->userid = $userid;
 		$this->amount = $amount;
 		$this->code = $code;
 		$this->section = $section;
+		$this->status = $status;
 	}
 
 }
