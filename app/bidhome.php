17f5be1ff97a04a4537f1dba7e233edacdd18f6d
Leonard <lywong.2017@sis.smu.edu.sg>
2019-10-06 19:25:43 +0800

    update bidhome.php

--- app/bidhome.php
+++ app/bidhome.php
@@ -13,6 +13,27 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #01579b">
+  <a class="navbar-brand" href="#" style="color:white;">BIOS: Bid Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="color:white;" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;" href="logout.php">Logout</a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Navigation Bar -->
+
 <?php
 
 include_once "include/common.php";
@@ -50,3 +71,16 @@ if ($round == 1)
 }
 
 ?>
+
+</div>
+		</div>
+	</div>
+	</main>
+	</body>
+
+<footer class="footer mt-auto py-3" style="background-color: #01579b;">
+  <div class="container">
+    <span style="float:left;color:white">&copy;2019 echo "T4";</span>
+	<span style="float:right;color:white;">All Rights Reserved</span>
+  </div>
+</footer>
