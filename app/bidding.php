f55d1ce84da493b49ab3e3d914e85eb64bf37a62
serene98 <serene.ang.2018@sis.smu.edu.sg>
2019-10-08 14:25:05 +0800

    Update bidding.php
    Update bidding.php to view sections for selected course and drop down list when student selects a section to bid.
    
    Co-Authored-By: one-ice <one-ice@users.noreply.github.com>

--- app/bidding.php
+++ app/bidding.php
@@ -1,5 +1,5 @@
 <?php
-include_once "include/common.php"; 
+include_once "app/include/common.php"; 
 include_once "meetCriteria.php";
 
 $course = $_GET['course'];
@@ -9,17 +9,50 @@ $studentedollar = ($studentDAO->retrieve($username))->edollar;
 if (!isset($_POST['submit']))
 {
     echo "<p> You have $$studentedollar.</p>";
+}
 
-    ?>
-    <html>
-        <body> <form method = 'post'>
-    <?php
-    echo "Course: <label for ='$course'>$course</label> <br>";
-    echo "Section: <input type = 'text' name = 'section'></input><br>";
-    echo "Bid Amount: <input type = 'text' name = 'bid_amt'></input> <br>";
+?>
+<html>
+    <body> <form method = 'post'>
+<?php
 
-    echo "<input type = 'submit' name = 'back' value = 'Back'> </input>";
-    echo "<input type = 'submit' name = 'submit' value = 'Submit'></input>";
+$days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+echo "Course: <label for = 'course'>$course</label> <br>";
+
+#Get section for selected course
+$sectionDAO = new SectionDAO();
+
+$sections = $sectionDAO->retrievebyCourse($course);
+if (count($sections) != 0)
+{
+    echo "<br><table border = '1'>
+        <tr> <th> Section </th> <th> Day </th> <th> Start </th>
+        <th> End </th> <th> Instructor </th> <th>	Venue </th> </tr>";
+    foreach ($sections as $section)
+    {
+        echo "<tr> <td> {$section->section} </td>
+                    <td> {$days[$section->day -1]} </td> 
+                    <td> {$section->start} </td>
+                    <td> {$section->end} </td>
+                    <td> {$section->instructor} </td>
+                    <td> {$section->venue} </td></tr>";
+    }
+
+    echo "</table><table border = '1'>";
+    echo "<tr> <td colspan  = '5'> Section: </td> <td> <select name = 'section' style='width: 80px'>";
+    foreach ($sections as $section)
+    {
+        echo " <option value = '$section->section'> $section->section </option>";
+    }
+    echo "</select></td></tr>";
+    echo "<tr> <td colspan = '5'> Bid Amount: </td> <td> <input type = 'text' name = 'bid_amt' style = 'width: 80px'></input> </td></tr></table>";
+    echo "<tr> <td> <input type = 'submit' name = 'back' value = 'Back' > </input> </td>";
+    echo "<td> <input type = 'submit' name = 'submit' value = 'Submit'></input> </td> </tr>";
+    echo"</table><br>";
+}
+else
+{
+    echo "<p> No sections available to bid </p>";
 }
 ?>
     </form> </body> 
