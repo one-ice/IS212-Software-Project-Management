31bb37127dc0f051d48477a55a6fa70e4e9686ac
SeeHoe <shlai.2018@sis.smu.edu.sg>
2019-10-09 22:44:21 +0800

    Updated RoundDAO.php to have retrieveStatus($round)
    Updated RoundDAO.php to have retrieveStatus($round)
    
    Co-Authored-By: tohjaslyn <tohjaslyn@users.noreply.github.com>

--- app/include/RoundDAO.php
+++ app/include/RoundDAO.php
@@ -40,6 +40,23 @@ class RoundDAO{
         return $isAddOK;
     }   
 
+    public  function retrieveStatus($round) {
+        $sql = 'SELECT status FROM round WHERE round=:round';
+            
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $result = "";
+
+        while($row = $stmt->fetch()) {
+            $result= new Round( $row['round'], $row['status']);
+        }        
+        return $result;
+    }
+
     
   
 }
