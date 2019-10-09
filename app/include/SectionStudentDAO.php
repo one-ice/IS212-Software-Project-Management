61894321aca7edd55437e88d71c5f0b66e9d0331
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2019-10-10 00:09:23 +0800

    Update SectionStudentDAO.php
    Added a retrieveVacancy to find the number of people enrolled in the section
    
    Co-Authored-By: seehoe <seehoe@users.noreply.github.com>

--- app/include/SectionStudentDAO.php
+++ app/include/SectionStudentDAO.php
@@ -19,25 +19,6 @@ class SectionStudentDAO {
         return $result;
     }
 
-    public function retrieveBySection($code, $section){
-        $sql = 'SELECT * FROM `section-student` WHERE code=:code and section=:section ORDER BY amount DESC';
-
-        $connMgr = new ConnectionManager();
-        $conn = $connMgr->getConnection();
-        $stmt = $conn->prepare($sql);
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $stmt->bindParam(':code', $code, PDO::PARAM_STR);
-        $stmt->bindParam(':section', $section, PDO::PARAM_STR);
-        $stmt->execute();
-        $result = array();
-
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $result[] = new SectionStudent($row['userid'], $row['code'], $row['section'], $row['amount']);
-        }
-        
-        return $result;
-    }
-    
     #get a distinct array of [code and section]
     public  function retrieveCodeSection() {
         $sql = 'SELECT DISTINCT code, section FROM `section-student` ORDER BY code, section';
@@ -54,6 +35,27 @@ class SectionStudentDAO {
             $result[] = [$row['code'] , $row['section']] ;
         }       
         return $result;
+    }
+
+    #get vacancy of section
+    public  function retrieveVacancy($code, $section) {
+        $sql = 'SELECT * FROM `section-student` WHERE code = :code and section = :section';
+        
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':code', $code, PDO::PARAM_STR);
+        $stmt->bindParam(':section', $section, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $result = array();
+
+        while($row = $stmt->fetch()) {
+            $result[] = [$row['code'] , $row['section']] ;
+        }      
+        $vacancy = sizeof($result); 
+        return $vacancy;
     }
 
     # $student is an object
