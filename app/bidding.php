a9cc4bd234dae128bcd7dc1a901ec223737a7f13
Leonard <lywong.2017@sis.smu.edu.sg>
2019-10-13 18:15:49 +0800

    Update bidding.php
    Added Navigation Bar

--- app/bidding.php
+++ app/bidding.php
@@ -13,6 +13,28 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
+
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #01579b">
+  <a class="navbar-brand" href="#" style="color:white;">BIOS: Bidding</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="color:white;" href="bidhome.php">Bid Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;" href="logout.php">Logout</a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Navigation Bar -->
+
 <?php
 include_once "include/common.php"; 
 include_once "meetCriteria.php";
