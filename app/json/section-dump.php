d657969bd80fbc7aa44e3a76d25f023184a3dbc2
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-10-19 17:30:06 +0800

    Created section-dump.php
    JSON web service

---
+++ app/json/section-dump.php
@@ -0,0 +1,37 @@
+<?php
+include_once "../include/common.php"; 
+
+$r = $_GET["r"];
+$obj = json_decode($r);
+$code = $obj->course;
+$section = $obj->section;
+$sectionStudentDAO = new SectionStudentDAO();
+$sectionStudentObj = $sectionStudentDAO->retrieveAllCourseAndSection($code,$section);
+if (sizeof($sectionStudentObj)>0) {
+$values = array();
+$value = array();
+ for($row = 0; $row < sizeof($sectionStudentObj); $row++)
+    {
+        $values[$row]['userid'] = $sectionStudentObj[$row]->userid;
+		$values[$row]['amount'] = $sectionStudentObj[$row]->amount;
+    }
+	
+	$result = [ 
+        "status" => "success",
+		"Students" => $values
+		
+    ];
+	header('Content-Type: application/json');
+	echo json_encode($result, JSON_PRETTY_PRINT);
+} 
+else {
+	$result = [ 
+            "status" => "error",
+            "message" => [ "invalid section" ]
+        ];
+	header('Content-Type: application/json');
+	echo json_encode($result, JSON_PRETTY_PRINT);
+}
+
+
+?>
