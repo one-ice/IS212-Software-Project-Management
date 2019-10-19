62b478d93e61e2c5fa2c344a7b5c7230dd5b4eef
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-10-19 18:11:45 +0800

    Update drop-section.php
    Update to check valid userid or not

--- app/json/drop-section.php
+++ app/json/drop-section.php
@@ -12,19 +12,37 @@ $roundInfo = $roundDAO->retrieveAll();
 $round = $roundInfo->round;
 $status = $roundInfo->status;
 
-#$studentDAO =  new StudentDAO();
+$studentDAO =  new StudentDAO();
+$student= "";
+$student = $studentDAO->retrieve($userid);
 #$sectionStudentDAO = new SectionStudentDAO();
 
-if ($round == 2 && $status == 'active') {
-$result = [ 
-        "status" => "error",
-		"message" => "round 2 active"
+if ($round == 2 && $status == 'active') {		#Check it is round 2 & active
+	
+	if (count((array)$student) > 0) {			#user id valid
+	
+	$result = [ 
+        "status" => "success",
+		"message" => "valid userid"
 		
     ];
 	header('Content-Type: application/json');
 	echo json_encode($result, JSON_PRETTY_PRINT);
+
+	}
+	 else {
+		$result = [ 
+        "status" => "error",
+		"message" => "invalid userid"
+		
+    ];
+	header('Content-Type: application/json');
+	echo json_encode($result, JSON_PRETTY_PRINT);
+	 }
 	
-} else {
+} 
+else 
+{
 	$result = [ 
         "status" => "error",
 		"message" => "round not active"
