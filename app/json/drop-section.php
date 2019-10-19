78d583096a03e8cbdaa77632005a04f692e8c55d
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-10-19 18:30:41 +0800

    update drop-section.php
    Validation to check if it is a valid course or not

--- app/json/drop-section.php
+++ app/json/drop-section.php
@@ -15,26 +15,39 @@ $status = $roundInfo->status;
 $studentDAO =  new StudentDAO();
 $student= "";
 $student = $studentDAO->retrieve($userid);
+$courseDAO = new CourseDAO();
+$course = "";
+$course = $courseDAO->retrieve($code);
 #$sectionStudentDAO = new SectionStudentDAO();
-
-if ($round == 2 && $status == 'active') {		#Check it is round 2 & active
+if ($round == 2 && $status == 'active') {		#round 2 & active
 	
 	if (count((array)$student) > 0) {			#user id valid
 	
-	$result = [ 
-        "status" => "success",
-		"message" => "valid userid"
+		if (is_Object($course) > 0) {
+			$result = [ 
+					"status" => "success",
+					"message" => "valid course"
+					];
+					header('Content-Type: application/json');
+					echo json_encode($result, JSON_PRETTY_PRINT);
+		}
+		else {
+
+			$result = [ 
+					"status" => "error",
+					"message" => "invalid course"
+					];
+					header('Content-Type: application/json');
+					echo json_encode($result, JSON_PRETTY_PRINT);
 		
-    ];
-	header('Content-Type: application/json');
-	echo json_encode($result, JSON_PRETTY_PRINT);
+		}
 
 	}
-	 else {
+	 else 
+	 {										#userid invalid
 		$result = [ 
         "status" => "error",
 		"message" => "invalid userid"
-		
     ];
 	header('Content-Type: application/json');
 	echo json_encode($result, JSON_PRETTY_PRINT);
@@ -42,11 +55,10 @@ if ($round == 2 && $status == 'active') {		#Check it is round 2 & active
 	
 } 
 else 
-{
+{											#Not Round 2 or not active or both
 	$result = [ 
         "status" => "error",
 		"message" => "round not active"
-		
     ];
 	header('Content-Type: application/json');
 	echo json_encode($result, JSON_PRETTY_PRINT);
