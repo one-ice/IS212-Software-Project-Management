9a70c9e6ef56088e0b45256c834a478c315dbd9f
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-10-19 19:13:50 +0800

    Updated section-dump.php
    Update with Validations to check valid course & section

--- app/json/section-dump.php
+++ app/json/section-dump.php
@@ -13,17 +13,25 @@ $code = $obj->course;
 $section = $obj->section;
 $sectionStudentDAO = new SectionStudentDAO();
 $sectionStudentObj = $sectionStudentDAO->retrieveAllCourseAndSection($code,$section);
-if (sizeof($sectionStudentObj)>0) {
-$values = array();
- for($row = 0; $row < sizeof($sectionStudentObj); $row++)
-    {
-        $values[$row]['userid'] = $sectionStudentObj[$row]->userid;
-		$values[$row]['amount'] = $sectionStudentObj[$row]->amount;
-    }
+$courseDAO = new CourseDAO();
+$course = "";
+$course = $courseDAO->retrieve($code);
+$sectionDAO = new SectionDAO();
+$sectionValid = $sectionDAO->retrieve($code,$section);
+if (is_Object($course) > 0) {
+	if (is_Object($sectionValid) > 0) {
+		
+	if (sizeof($sectionStudentObj)>0) {
+		$values = array();
+		for($row = 0; $row < sizeof($sectionStudentObj); $row++)
+		{
+			$values[$row]['userid'] = $sectionStudentObj[$row]->userid;
+			$values[$row]['amount'] = $sectionStudentObj[$row]->amount;
+		}
 	
 	$result = [ 
         "status" => "success",
-		"Students" => $values
+		"students" => $values
 		
     ];
 	header('Content-Type: application/json');
@@ -36,6 +44,25 @@ else {
         ];
 	header('Content-Type: application/json');
 	echo json_encode($result, JSON_PRETTY_PRINT);
+}
+
+
+	} else {
+			$result = [ 
+			"status" => "error",
+			"message" => "invalid section"
+			];
+			header('Content-Type: application/json');
+			echo json_encode($result, JSON_PRETTY_PRINT);
+	}
+}
+else {
+	$result = [ 
+			"status" => "error",
+			"message" => "invalid course"
+			];
+			header('Content-Type: application/json');
+			echo json_encode($result, JSON_PRETTY_PRINT);
 }
 
 
