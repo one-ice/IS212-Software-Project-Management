13c60ee4eafb801fd4c6bea375b830abf7774a9b
one-ice <53385835+one-ice@users.noreply.github.com>
2019-10-27 21:01:32 +0800

    Update protect_json.php

---
+++ app/json/protect_json.php
@@ -0,0 +1,46 @@
+<?php
+require_once '../include/token.php';
+require_once '../include/common.php';
+
+$token = '';
+$errors = [];
+
+#for json_testing
+if  (isset($_REQUEST['token'])) {
+  $token = $_REQUEST['token'];
+}else{
+  $errors[] = 'missing token';
+  }
+
+// #for web
+// if  (isset($_SESSION['token'])) {
+//   $token = $_SESSION['token'];
+// }
+if($token==""){
+  $errors[] = 'blank token';
+  }
+# check if token is not valid
+# reply with appropriate JSON erro  r message
+if (verify_token($token) != 'admin'){
+  $errors[] = 'invalid token';
+}
+
+# add your code here
+# this bit below might be useful for protecting the JSON requests and for your project 
+# it will help to check for more conditions such as 
+
+# if the user is not an admin and trying to access admin pages
+
+# if the user is trying to access json services and is not doing it properly
+
+# $pathSegments = explode('/',$_SERVER['PHP_SELF']); # Current url
+# $numSegment = count($pathSegments);
+# $currentFolder = $pathSegments[$numSegment - 2]; # Current folder
+# $page = $pathSegments[$numSegment -1]; # Current page
+
+# you can do things like If ($page == "bootstrap-view.php) {   or 
+# if ($currentfolder == "json") {  
+
+
+
+?>
