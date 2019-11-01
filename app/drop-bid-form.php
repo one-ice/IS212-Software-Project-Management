7d3b3fdb78216871d11e430954c6a8b59107dad5
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-11-01 22:28:49 +0800

    drop-bid-home navigation bar update

--- app/drop-bid-form.php
+++ app/drop-bid-form.php
@@ -1,7 +1,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>BIOS: Drop Bid</title>
+<title>BIOS: Bidding</title>
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -15,7 +15,7 @@
 
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #01579b">
-  <a class="navbar-brand" href="#" style="color:white;">BIOS: Drop Bid</a>
+  <a class="navbar-brand" href="bidhome.php" style="color:white;">BIOS: Drop Bid</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,7 +23,16 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" style="color:white;" href="bidhome.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" style="color:white;" href="bidhome.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="color:white;" href="drop-bid-form.php">Drop Bid<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="color:white;" href="drop_section.php">Drop Section<span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" style="color:white;" href="bidding_status.php">Bidding Status</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" style="color:white;" href="logout.php">Logout</a>
