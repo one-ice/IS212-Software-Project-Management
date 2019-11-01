b14678b15930a8705b6dda693a53b1f104236510
Leonard Wong <lywong.2017@sis.smu.edu.sg>
2019-11-01 22:27:19 +0800

    Update drop_section.php
    Update Heading

--- app/drop_section.php
+++ app/drop_section.php
@@ -15,7 +15,7 @@
 
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #01579b">
-  <a class="navbar-brand" href="bidhome.php" style="color:white;">BIOS: Bidding</a>
+  <a class="navbar-brand" href="bidhome.php" style="color:white;">BIOS: Drop Section</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
